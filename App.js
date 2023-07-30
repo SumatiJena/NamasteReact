ddb4ce5ada4531beedf7e7bddcfb48338f352612
SumatiJena <98972407+SumatiJena@users.noreply.github.com>
2023-07-30 19:18:10 +0530

    create component

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import React from "react";
-import  ReactDOM from "react-dom";
-const heading=React.createElement("h1",{},"Hello World from React");
+import  ReactDOM from "react-dom/client";
+const Title=()=>(
+ <h1>this is title</h1>
+)
+const Heading=()=>{
+    return <div>
+        <Title/>
+        <h1>This is heading for jsx</h1>
+    </div>
+}
+   
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
+root.render(<Heading/>)
